eee576ea3b1a73886054f54c1a2a05bc53fdf1d4
ねじくぎ <63852665+scrwnl@users.noreply.github.com>
2025-02-20 16:03:37 +0000

    ファイルパスを修正

--- genver.sh
+++ genver.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/bash
+BUILD_DIR=build
+mkdir -p $BUILD_DIR
 export COMMIT_ID=$(git show --format=\'\%H\' --no-patch)
-cat << EOF > version.ts
+cat << EOF > ${BUILD_DIR}/version.ts
 export const version = ${COMMIT_ID}
 EOF
